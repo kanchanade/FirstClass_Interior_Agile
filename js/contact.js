4b342f034254c6cf81e3ae39deed383a589731a2
Kanchana De Silva <kanchanade@gmail.com>
2021-10-29 11:19:43 +1300

    Commit files

---
+++ js/contact.js
@@ -0,0 +1,7 @@
+function alignContactUs(){
+    document.getElementById('address').classList.add("text-start");
+}
+function changeContactUs(){
+    document.getElementById('contact-header').classList.add("text-success");
+    document.getElementById('contact-header').classList.add("fs-2");
+}
