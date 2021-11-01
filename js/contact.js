754dab295b617666492875ac2a56abe8082ad690
kanchanade <92826875+kanchanade@users.noreply.github.com>
2021-11-02 11:25:25 +1300

    Delete js directory

--- js/contact.js
+++
@@ -1,7 +0,0 @@
-function alignContactUs(){
-    document.getElementById('address').classList.add("text-start");
-}
-function changeContactUs(){
-    document.getElementById('contact-header').classList.add("text-success");
-    document.getElementById('contact-header').classList.add("fs-2");
-}
